1abdbd8cc08558ca719312245854cd64944e92c5
tvongerlach <tassilo.v.gerlach@gmail.com>
2016-11-01 19:23:02 -0700

    Installed pods  and implemented UI

---
+++ PokeFind/PokeFinder-Brdiging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFind-Brdiging-HEader.h
+//  PokeFind
+//
+//  Created by Tassilo von Gerlach on 11/1/16.
+//  Copyright © 2016 Tassilo von Gerlach. All rights reserved.
+//
+
+#import "GeoFire.h"
